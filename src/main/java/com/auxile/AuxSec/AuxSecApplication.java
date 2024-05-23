a41bd4e3a42368acd0dd11268003807604f23809
Auxile-Isaac <auxile.isaac@gmail.com>
2024-05-23 23:49:43 +0200

    initial commit

---
+++ src/main/java/com/auxile/AuxSec/AuxSecApplication.java
@@ -0,0 +1,13 @@
+package com.auxile.AuxSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuxSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuxSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auxile/AuxSec/AuxSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auxile.AuxSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuxSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
